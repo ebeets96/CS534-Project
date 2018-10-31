9917cec6984736886d935bea95e5138d58ae9133
Carol Clancy <caroline-clancy@Carols-MacBook-Air.local>
2018-10-30 19:31:39 -0500

    added height and width parameters to function

--- resize_image.py
+++ resize_image.py
@@ -1,17 +1,17 @@
 import numpy as np
 import cv2
 
-def resize_image(img):
+def resize_image(img, w, h):
 	height = img.shape[0]
 	width = img.shape[1]
 
-	if height < 400 or width < 400:
+	if height < h or width < w:
 		raise ValueError("Please enter an image with a height of at least 400 and a width of at least 400")
 
 	scaled_img = img
-	if (height - 400) <= (width - 400):
-		new_width = int((400/height)*width)
-		scaled_img = cv2.resize(img, (new_width, 400))
+	if (height - h) <= (width - w):
+		new_width = int((h/height)*width)
+		scaled_img = cv2.resize(img, (new_width, w))
 		i = 0
 		left = 0
 		right = new_width
@@ -24,20 +24,20 @@ def resize_image(img):
 			total_height = right - left
 		crop_img = scaled_img[0:400, left:right]
 	else:
-		new_height = int(400/width)*height
-		scaled_img = cv2.resize(img, (400, new_height))
+		new_height = int(w/width)*height
+		scaled_img = cv2.resize(img, (h, new_height))
 		i = 0
 		top = 0
 		bottom = new_height
 		total_height = new_height
-		while (total_height != 400):
+		while (total_height != h):
 			if i % 2 == 0:
 				top += 1
 			else:
 				bottom -= 1
 			total_height = bottom - top 			
 		
-		crop_img = scaled_img[top:bottom, 0:400]
+		crop_img = scaled_img[top:bottom, 0:w]
 
 #	cv2.imshow("Display Window", scaled_img)
 #	cv2.imshow("Display Window", crop_img)
